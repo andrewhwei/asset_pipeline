4a114f151adc43e7bd1cc55e065762e0ab24de02
Andrew Wei <andrew.wei92@yahoo.com>
2016-05-08 13:45:13 -0700

    fixed errors regarding assets

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'pages', to: 'pages#index'
 
+  get 'portfolios', to: 'portfolios#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
